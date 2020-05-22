25e69694bb7e71a57af316c488aaae8650f0fa9a
maleeshasilva <56461176+maleeshasilva@users.noreply.github.com>
2020-05-22 11:25:45 +0530

    Add files via upload

---
+++ Ques2.scala
@@ -0,0 +1,19 @@
+object Ques3{
+
+	def main(args:Array[String])
+	{	
+	println("Volume is :"+functionToVolume(5));	
+	}
+
+	def functionToVolume(r:Int):Double= {
+	
+	var volume:Double=0;
+
+	volume=(4.0/3.0)*math.Pi*r*r*r	
+
+	
+	return volume
+	}
+
+
+}
