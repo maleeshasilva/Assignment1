08049bb1ed1fa6f734de1c05ad2840428fa0960c
maleeshasilva <56461176+maleeshasilva@users.noreply.github.com>
2020-07-19 11:25:21 +0530

    Assignment 4

---
+++ Ass4Ques1.scala
@@ -0,0 +1,26 @@
+object Ass4Ques1{
+
+def main(args:Array[String])
+{
+val alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+val Encryption=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)+key)%a.size)
+//encryption
+
+val Decryption =(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key+a.size)%a.size)
+//decryption
+
+val cipher=(algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
+
+
+val ct=cipher(Encryption,"secret",5,alphabet)
+
+val pt=cipher(Decryption,"secret",5,alphabet)
+
+println("The result of the  Encryption :" +ct)
+
+println("The result of  the  Decryption :" +pt)
+
+}
+
+}
