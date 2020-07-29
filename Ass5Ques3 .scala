cf20f05876f0d21f1ee253b7be9835ad62011816
maleeshasilva <56461176+maleeshasilva@users.noreply.github.com>
2020-07-29 14:25:54 +0530

    Assignment 5 Question 3

--- Ass5Ques3 .scala
+++ Ass5Ques3 .scala
@@ -3,8 +3,19 @@ class Account (NIC:String,accno:Int,b:Double) {
   def accountnumber=accno
   def balance=b
   
-  def transfer(m:Double)=
-  { this.balance +m }
+  def withdraw(l:Double)={
+    this.balance-l
+  }
+  def deposit(n:Double)={
+    this.balance+n
+  }
+  
+  def transfer(a:Account,m:Double)=
+  { this.withdraw(m) 
+    a.deposit(m)
+   println("A balance :"+this.withdraw(m))
+   println("B balance :"+a.withdraw(m))
+  }
   
  
   override def toString ="["+NIC+":"+accountnumber +":"+ balance+"]" 
@@ -14,18 +25,18 @@ object Ass5Ques3 extends App {
   val a = new Account ("967412503v",101,1200.00)
   val b = new Account ("972505032v",102,14500.33)
  
-  val m:Double=5200.35
-  val n:Double=1000.48
+  val m:Double=1000.00
   
-  println("Your Account: "+a)
+  println("Your Account A: "+a)
+  println("Your Account B: "+b)
+  println(" ")  
+  println("Money is transfered from A to B ")
   println("Transfered value :"+m)
-  println("Final Balance: "+a.transfer(m))
-  
-  println(" ")
-  println("Your Account: "+b)
-  println("Transfered value :"+n)
-  println("Final Balance: "+b.transfer(n))
- 
+  println("Final Result :")
+  a.transfer(b,m)
+
 
 }
+
+
 
