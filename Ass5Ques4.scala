6854a0d51ad764d98d7c2cdc15765a9c8efcdca8
maleeshasilva <56461176+maleeshasilva@users.noreply.github.com>
2020-07-30 19:53:05 +0530

    Assignment 5 Question 4

--- Ass5Ques4.scala
+++ Ass5Ques4.scala
@@ -2,52 +2,49 @@ class Account(NIC: String, accno: Int, b: Double) {
   def NIC: Any = NIC
   def accountnumber = accno
   def balance = b
-
-  def withdraw(l: Double) = {
-    this.balance - l
-  }
-  def deposit(n: Double) = {
-    this.balance + n
-  }
-
-  def transfer(a: Account, m: Double) = {
-    this.withdraw(m)
-    a.deposit(m)
-    println("A balance :" + this.withdraw(m))
-    println("B balance :" + a.withdraw(m))
-  }
-
-  override def toString = "[" + NIC + ":" + accountnumber + ":" + balance + "]"
+  
 }
 
 object Ass5Ques4 extends App {
 
-  val a = new Account("967412503v", 101, 1200.00)
-  val b = new Account("972505032v", 102, 14500.33)
-  val c = new Account("992007802v", 103, -125.00)
-  val d = new Account("964582138v", 104, -1025.00)
-  val e = new Account("789654802v", 106, 84785.00)
-
-  val bank: List[Account] = List(a, b, c, d, e)
-  
-
-  //  question 1 :- List of accounts with negative balances
+   
   val overdraft = (bank: List[Account]) => bank.filter(x => x.balance < 0)
-  println("Ques1:- List of accounts with negative balances :")
-  println(" ")
-  println(overdraft(bank))
-  
+  val Accbalance = (bank: List[Account]) =>bank.reduce((x, y) => new Account("V000000000", 1000, x.balance + y.balance))
+  val interest = (bank: List[Account]) =>bank.map(y =>(if (y.balance > 0) y.balance * 1.0005d else y.balance * 1.001d))
 
 
-  //qustion 2:-total balances
-  val balance =(b:List[Account])=>Double={b.reduce((x,y)=>(x.balance) + (y.balance))}
-  println(" Total balance of the Accounts: "+balance(bank)) 
-  
-  
- //question 3:- Interest
-  val f1 =(b:List[Account])=>for(i<-b){if(b(i).balance<0) b(i).balance *1.0005  else b(i).balance* 1.001}
-  val interest=(b:List[Account])=>b.map(f1)
-  println("After the addition of interest :") 
-  println(interest(bank))                               
+    println("---------------------------------------------- ")
+    
+   
+    val a = new Account("967412503v", 101, 1200.00);
+    val b = new Account("972505032v", 102, 14500.33);
+    val c = new Account("992007802v", 103, -125.00);
+    val d = new Account("964582138v", 104, -1025.00);
+    val e = new Account("789654802v", 106, 84785.00);
+    val f = new Account("652389412v", 107, 485962.00);
 
+    val bank: List[Account] = List(a, b, c, d, e, f)
+
+    //  question 1 :- List of accounts with negative balances
+
+    println("Ques1:- List of accounts with negative balances :")
+    var overdraftlist = overdraft(bank)
+    overdraftlist.foreach(x => print(x.accountnumber + "   "));
+     println(" ")
+
+    //qustion 2:-total balances
+   
+    var sum = Accbalance(bank)
+    println(" ")
+    println("Ques2:-  Total balance of the Accounts: " + sum.balance)
+
+    //question 3:- Account balance adding Interest
+    println(" ")
+    println("Ques3:- Account balance adding Interest :")
+    var interestlist = interest(bank)
+    interestlist.foreach(i => print(i + " "))
+
+
+
+  
 }
